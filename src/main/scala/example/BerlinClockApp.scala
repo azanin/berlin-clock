500dda3abfba19405a05a155f28c982ae607d5c8
azanin <ale.zanin90@gmail.com>
2021-01-31 13:59:41 +0100

    implement single hours row

--- src/main/scala/example/BerlinClockApp.scala
+++ src/main/scala/example/BerlinClockApp.scala
@@ -4,19 +4,25 @@ object BerlinClockApp extends App {}
 
 object Converter {
 
-  val panelFiveMinutesSize   = 11
-  val panelSingleMinutesSize = 4
+  val singleHourRowSize    = 4
+  val fiveMinutesRowSize   = 11
+  val singleMinutesRowSize = 4
 
   def convertFiveMinute(minutes: Int): String = {
     val nLedOn = minutes / 5
-    build(nLedOn / 3, "YYR") + build(nLedOn % 3, "Y") + build(panelFiveMinutesSize - nLedOn, "O")
+    build(nLedOn / 3, "YYR") + build(nLedOn % 3, "Y") + build(fiveMinutesRowSize - nLedOn, "O")
+  }
+
+  def convertSingleHour(hours: Int): String = {
+    val nLedOn = hours % 5
+    build(nLedOn, "R") + build(singleHourRowSize - nLedOn, "O")
+  }
+
+  def convertSingleMinute(minutes: Int): String = {
+    val nLedOn = minutes % 5
+    build(nLedOn, "Y") + "O".repeat(singleMinutesRowSize - nLedOn)
   }
 
   private def build(lenght: Int, pattern: String): String =
     pattern.repeat(lenght)
-
-  def convertSingleMinute(minutes: Int): String = {
-    val nLedOn = minutes % 5
-    build(nLedOn, "Y") + "O".repeat(panelSingleMinutesSize - nLedOn)
-  }
 }
--- src/test/scala/example/ConverterTest.scala
+++ src/test/scala/example/ConverterTest.scala
@@ -23,6 +23,15 @@ class ConverterTest extends AnyFunSuite with TableDrivenPropertyChecks {
     (35, "YYRYYRYOOOO")
   )
 
+  val singleHourExamples = Table(
+    ("hours", "representation"),
+    (0, "OOOO"),
+    (23, "RRRO"),
+    (2, "RROO"),
+    (8, "RRRO"),
+    (14, "RRRR")
+  )
+
   forAll(singleMinutesExamples) { (input, expected) =>
     test(s"single minutes row for input: $input") {
       val actual = Converter.convertSingleMinute(input)
@@ -33,6 +42,13 @@ class ConverterTest extends AnyFunSuite with TableDrivenPropertyChecks {
   forAll(fiveMinutesExamples) { (input, expected) =>
     test(s"five minutes row for input: $input") {
       val actual = Converter.convertFiveMinute(input)
+      assert(actual == expected)
+    }
+  }
+
+  forAll(singleHourExamples) { (input, expected) =>
+    test(s"single hours row for input: $input") {
+      val actual = Converter.convertSingleHour(input)
       assert(actual == expected)
     }
   }
