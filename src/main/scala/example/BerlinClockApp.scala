33f393f2ed07cf7014f7e1c5f6f040e6dc1547b7
azanin <ale.zanin90@gmail.com>
2021-01-31 14:23:17 +0100

    integrate berlin clock

--- src/main/scala/example/BerlinClockApp.scala
+++ src/main/scala/example/BerlinClockApp.scala
@@ -1,12 +1,19 @@
 package example
 
+import java.time.LocalTime
+
 object BerlinClockApp extends App {}
 
 object Converter {
-
+  val fiveHourRowSize      = 4
   val singleHourRowSize    = 4
   val fiveMinutesRowSize   = 11
   val singleMinutesRowSize = 4
+
+  def convertSeconds(seconds: Int) = {
+    val nLedOn = seconds % 2
+    build(1 - nLedOn, "Y") + build(nLedOn, "O")
+  }
 
   def convertFiveMinute(minutes: Int): String = {
     val nLedOn = minutes / 5
@@ -22,6 +29,18 @@ object Converter {
     val nLedOn = minutes % 5
     build(nLedOn, "Y") + "O".repeat(singleMinutesRowSize - nLedOn)
   }
+
+  def convertFiveHours(hours: Int) = {
+    val nLedOn = hours / 5
+    build(nLedOn, "R") + build(fiveHourRowSize - nLedOn, "O")
+  }
+
+  def convertTime(input: LocalTime) =
+    convertSeconds(input.getSecond) +
+      convertFiveHours(input.getHour) +
+      convertSingleHour(input.getHour) +
+      convertFiveMinute(input.getMinute) +
+      convertSingleMinute(input.getMinute)
 
   private def build(lenght: Int, pattern: String): String =
     pattern.repeat(lenght)
--- src/test/scala/example/ConverterTest.scala
+++ src/test/scala/example/ConverterTest.scala
@@ -1,5 +1,7 @@
 package example
 
+import java.time.LocalTime
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.prop.TableDrivenPropertyChecks
 
@@ -32,6 +34,29 @@ class ConverterTest extends AnyFunSuite with TableDrivenPropertyChecks {
     (14, "RRRR")
   )
 
+  val fiveHourExamples = Table(
+    ("hours", "representation"),
+    (0, "OOOO"),
+    (23, "RRRR"),
+    (2, "OOOO"),
+    (8, "ROOO"),
+    (16, "RRRO")
+  )
+
+  val secondsExamples = Table(
+    ("seconds", "representation"),
+    (0, "Y"),
+    (59, "O")
+  )
+
+  val timeExamples = Table(
+    ("time", "representation"),
+    (LocalTime.of(0, 0, 0), "YOOOOOOOOOOOOOOOOOOOOOOO"),
+    (LocalTime.of(23, 59, 59), "ORRRRRRROYYRYYRYYRYYYYYY"),
+    (LocalTime.of(16, 50, 6), "YRRROROOOYYRYYRYYRYOOOOO"),
+    (LocalTime.of(11, 37, 1), "ORROOROOOYYRYYRYOOOOYYOO")
+  )
+
   forAll(singleMinutesExamples) { (input, expected) =>
     test(s"single minutes row for input: $input") {
       val actual = Converter.convertSingleMinute(input)
@@ -51,6 +76,28 @@ class ConverterTest extends AnyFunSuite with TableDrivenPropertyChecks {
       val actual = Converter.convertSingleHour(input)
       assert(actual == expected)
     }
+  }
+
+  forAll(fiveHourExamples) { (input, expected) =>
+    test(s"five hours row for input: $input") {
+      val actual = Converter.convertFiveHours(input)
+      assert(actual == expected)
+    }
+  }
+
+  forAll(secondsExamples) { (input, expected) =>
+    test(s"seconds row for input: $input") {
+      val actual = Converter.convertSeconds(input)
+      assert(actual == expected)
+    }
+  }
+
+  forAll(timeExamples) { (input, expected) =>
+    test(s"time $input should be: $expected") {
+      val actual = Converter.convertTime(input)
+      assert(actual == expected)
+    }
+
   }
 
 }
